7f0f3ad61e4e709469adcd4be304d21021ea90b2
Takanori TANIGUCHI <askme@pretendco.net>
2019-03-29 14:42:18 +0900

    update year of copyright

--- macOSUpgrade.sh
+++ macOSUpgrade.sh
@@ -2,7 +2,7 @@
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
-# Copyright (c) 2018 Jamf.  All rights reserved.
+# Copyright (c) 2019 Jamf.  All rights reserved.
 #
 #       Redistribution and use in source and binary forms, with or without
 #       modification, are permitted provided that the following conditions are met:
@@ -35,7 +35,7 @@
 # as well as to address changes Apple has made to the ability to complete macOS upgrades
 # silently.
 #
-# VERSION: v2.7.3
+# VERSION: v2.7.3.1
 #
 # REQUIREMENTS:
 #           - Jamf Pro
